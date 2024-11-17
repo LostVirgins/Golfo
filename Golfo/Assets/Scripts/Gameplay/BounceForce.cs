79a0f5d1e2c1ec758947632df256ab5f19cd84e5
hekbas <hekbas@gmail.com>
2024-11-17 21:47:14 +0100

    Fixed LineForce Script cheking input on FixedUpdate
    - conventions and namespaces
    - code clean

--- Golfo/Assets/Scripts/Gameplay/BounceForce.cs
+++ Golfo/Assets/Scripts/Gameplay/BounceForce.cs
@@ -1,57 +1,60 @@
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class BounceForce : MonoBehaviour
+namespace lv.gameplay
 {
-    [SerializeField] private float m_bouncePower;
-    [SerializeField][Range(0,1)] private float m_restitution;
-    private Vector3 testVar;
-    private Vector3 collPos;
-    private Vector3 normPos;
-    private Vector3 lastVel;
-    
-
-    private Rigidbody m_rigidbody;
-
-    private void Awake()
+    public class BounceForce : MonoBehaviour
     {
-        m_rigidbody = GetComponent<Rigidbody>();
-        testVar = new Vector3(0,0,0);
-        collPos = new Vector3(0,0,0);
-        normPos = new Vector3(0,0,0);
-        lastVel = new Vector3(0,0,0);
-    }
+        [SerializeField] private float m_bouncePower;
+        [SerializeField][Range(0, 1)] private float m_restitution;
+        private Vector3 testVar;
+        private Vector3 collPos;
+        private Vector3 normPos;
+        private Vector3 lastVel;
 
-    private void Update()
-    {
-        // Debug Vectors
-        //Debug.DrawLine(m_rigidbody.position, m_rigidbody.velocity * 100, Color.red);
-        //Debug.DrawLine(collPos, testVar * 100, Color.green);
-        //Debug.DrawLine(collPos, normPos * 100, Color.magenta);
-        
-        lastVel = m_rigidbody.velocity;
-    }
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.tag == "Wall")
+
+        private Rigidbody m_rigidbody;
+
+        private void Awake()
         {
-            Vector3 wallNormal = collision.GetContact(0).normal.normalized;
-            BallBounce(wallNormal);
+            m_rigidbody = GetComponent<Rigidbody>();
+            testVar = new Vector3(0, 0, 0);
+            collPos = new Vector3(0, 0, 0);
+            normPos = new Vector3(0, 0, 0);
+            lastVel = new Vector3(0, 0, 0);
+        }
+
+        private void Update()
+        {
+            // Debug Vectors
+            //Debug.DrawLine(m_rigidbody.position, m_rigidbody.velocity * 100, Color.red);
+            //Debug.DrawLine(collPos, testVar * 100, Color.green);
+            //Debug.DrawLine(collPos, normPos * 100, Color.magenta);
+
+            lastVel = m_rigidbody.velocity;
+        }
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.tag == "Wall")
+            {
+                Vector3 wallNormal = collision.GetContact(0).normal.normalized;
+                BallBounce(wallNormal);
+
+                // Debug Vectors
+                normPos = wallNormal;
+            }
+        }
+
+        private void BallBounce(Vector3 wallNormal)
+        {
+            //vref = vi -2 * (vi * normal) * normal
+            Vector3 dir = Vector3.Reflect(new Vector3(lastVel.x, 0, lastVel.z), wallNormal);
 
             // Debug Vectors
-            normPos = wallNormal;
+            testVar = dir;
+            collPos = m_rigidbody.position;
+
+            m_rigidbody.velocity = dir;
         }
-    }
-
-    private void BallBounce(Vector3 wallNormal)
-    {
-        //vref = vi -2 * (vi * normal) * normal
-        Vector3 dir = Vector3.Reflect(new Vector3(lastVel.x, 0, lastVel.z), wallNormal);
-
-        // Debug Vectors
-        testVar = dir;
-        collPos = m_rigidbody.position;
-
-        m_rigidbody.velocity = dir;
     }
 }
--- Golfo/Assets/Scripts/Gameplay/LineForce.cs
+++ Golfo/Assets/Scripts/Gameplay/LineForce.cs
@@ -1,100 +1,106 @@
 using UnityEngine;
 
-public class LineForce : MonoBehaviour {
-    [SerializeField] private float shotPower;
-    [SerializeField] private float stopVelocity = .05f; //The velocity below which the rigidbody will be considered as stopped
+namespace lv.gameplay
+{
+    public class LineForce : MonoBehaviour
+    {
+        [SerializeField] private float shotPower;
+        [SerializeField] private float stopVelocity = 0.15f; //The velocity below which the rigidbody will be considered as stopped
 
-	[SerializeField] private LineRenderer lineRenderer;
+        [SerializeField] private LineRenderer lineRenderer;
 
-    private bool isIdle;
-    private bool isAiming;
+        private bool isIdle;
+        private bool isAiming;
 
-    private Rigidbody rigidbody;
+        private Rigidbody rigidbody;
 
-    private void Awake() {
-        rigidbody = GetComponent<Rigidbody>();
+        private void Awake()
+        {
+            rigidbody = GetComponent<Rigidbody>();
 
-        isAiming = false;
-        lineRenderer.enabled = false;
-    }
-
-    private void FixedUpdate() {
-        if(rigidbody.velocity.magnitude < stopVelocity) {
-            Stop();
+            isAiming = false;
+            lineRenderer.enabled = false;
         }
 
-        ProcessAim();
-    }
+        private void Update()
+        {
+            if (rigidbody.velocity.magnitude < stopVelocity)
+                Stop();
 
-    private void OnMouseDown() {
-        if (isIdle) {
-            isAiming = true;
-        }
-    }
-
-    private void ProcessAim() {
-        if(!isAiming || !isIdle) {
-            return;
+            ProcessAim();
         }
 
-        Vector3? worldPoint = CastMouseClickRay();
-
-        if (!worldPoint.HasValue) {
-            return;
+        private void OnMouseDown()
+        {
+            if (isIdle)
+                isAiming = true;
         }
 
-        DrawLine(worldPoint.Value);
+        private void ProcessAim()
+        {
+            if (!isAiming || !isIdle)
+                return;
 
-        if (Input.GetMouseButtonUp(0)) {
-            Shoot(worldPoint.Value);
+            Vector3? worldPoint = CastMouseClickRay();
+
+            if (!worldPoint.HasValue)
+                return;
+
+            DrawLine(worldPoint.Value);
+
+            if (Input.GetMouseButtonUp(0))
+                Shoot(worldPoint.Value);
         }
-    }
 
-    private void Shoot(Vector3 worldPoint) {
-        isAiming = false;
-        lineRenderer.enabled = false;
+        private void Shoot(Vector3 worldPoint)
+        {
+            isAiming = false;
+            lineRenderer.enabled = false;
 
-        Vector3 horizontalWorldPoint = new Vector3(worldPoint.x, transform.position.y, worldPoint.z);
+            Vector3 horizontalWorldPoint = new Vector3(worldPoint.x, transform.position.y, worldPoint.z);
 
-        Vector3 direction = (horizontalWorldPoint - transform.position).normalized;
-        float strength = Vector3.Distance(transform.position, horizontalWorldPoint);
+            Vector3 direction = (horizontalWorldPoint - transform.position).normalized;
+            float strength = Vector3.Distance(transform.position, horizontalWorldPoint);
 
-        rigidbody.AddForce(-direction * strength * shotPower);
-        isIdle = false;
-    }
+            rigidbody.AddForce(-direction * strength * shotPower);
+            isIdle = false;
+        }
 
-    private void DrawLine(Vector3 worldPoint) {
-        Vector3[] positions = {
+        private void DrawLine(Vector3 worldPoint)
+        {
+            Vector3[] positions = {
             new Vector3(transform.position.x, transform.position.y, transform.position.z),
             new Vector3(worldPoint.x, transform.position.y, worldPoint.z)};
-        lineRenderer.SetPositions(positions);
-        lineRenderer.enabled = true;
+            lineRenderer.SetPositions(positions);
+            lineRenderer.enabled = true;
+        }
+
+        private void Stop()
+        {
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.angularVelocity = Vector3.zero;
+            isIdle = true;
+        }
+
+        private Vector3? CastMouseClickRay()
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var plane = new Plane(Vector3.up, rigidbody.position);
+
+            // Debug Direction Ray
+            //Debug.DrawRay(rigidbody.position, direction * 100, Color.red);
+
+            float rayDistance;
+            if (plane.Raycast(ray, out rayDistance))
+            {
+                // Debug Shot Ray
+                //Debug.DrawRay(rigidbody.position, ray.GetPoint(rayDistance), Color.yellow);
+
+                return ray.GetPoint(rayDistance);
+            }
+
+            return null;
+        }
     }
 
-    private void Stop() {
-        rigidbody.velocity = Vector3.zero;
-        rigidbody.angularVelocity = Vector3.zero;
-        isIdle = true;
-    }
-
-    private Vector3? CastMouseClickRay() {
-
-        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        var plane = new Plane(Vector3.up, rigidbody.position);
-
-        // Debug Direction Ray
-        //Debug.DrawRay(rigidbody.position, direction * 100, Color.red);
-
-        float rayDistance;
-        if (plane.Raycast(ray, out rayDistance)) {
-
-            // Debug Shot Ray
-            //Debug.DrawRay(rigidbody.position, ray.GetPoint(rayDistance), Color.yellow);
-
-            return ray.GetPoint(rayDistance);
-        }        
-
-        return null;
-
-    }
 }
