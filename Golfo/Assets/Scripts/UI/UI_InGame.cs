53d77988aba0dacb8a1e9ad47647642a0be56f74
Francisco Ferrerós Puigmarti <99959433+kikofp02@users.noreply.github.com>
2025-01-01 17:48:36 +0100

    Game Ui Updates
    - Finally solved issue with ui interactions
    - First draft of Score Workflow on script UI_InGame

--- Golfo/Assets/Scripts/UI/UI_InGame.cs
+++ Golfo/Assets/Scripts/UI/UI_InGame.cs
@@ -1,4 +1,8 @@
+using lv.gameplay;
+using lv.network;
+using System.Collections.Generic;
 using TMPro;
+using UnityEditor.VersionControl;
 using UnityEngine;
 
 namespace lv.ui
@@ -6,6 +10,13 @@ namespace lv.ui
     public class UI_InGame : MonoBehaviour
     {
         public static UI_InGame Instance { get; private set; }
+
+        NetworkManager networkManager = NetworkManager.Instance;
+        GameManager gameManager = GameManager.Instance;
+
+        public GameObject m_scoreSec;
+        public GameObject m_scoreNodePrefab;
+        public GameObject m_scoreViewContent;
 
         public GameObject m_debugSec;
         public GameObject m_debugMessagePrefab;
@@ -22,7 +33,69 @@ namespace lv.ui
                 TogleDebugWindow();
         }
 
-    #region DebugWindow
+        #region ScoreWindow
+
+        public void TogleScoreWindow()
+        {
+            m_scoreSec.SetActive(!m_debugSec.activeSelf);
+
+            if(m_scoreSec.activeSelf)
+                OpenScoreWindow();
+            else
+                CloseScoreWindow();
+        }
+
+        void CloseScoreWindow()
+        {
+            // borrar todos los children de m_scoreViewContent
+        }
+
+        void OpenScoreWindow()
+        {
+            // Esto esta mal, faltan pasos
+                    //Primero ordenar la lista de players por score, luego añadir los node por cada player
+            foreach (var player in networkManager.m_players)
+            {
+                AddScoreNode(player);
+            }
+        }
+
+
+        void AddScoreNode(Player player)
+        {
+            GameObject messageObject = Instantiate(m_debugMessagePrefab, m_debugViewContent.transform);
+
+            List<GameObject> children = gameManager.GetAllChildren(messageObject);
+
+            int i = 0;
+            foreach (GameObject child in children)
+            {
+                TextMeshProUGUI messageTextComponent = child.GetComponent<TextMeshProUGUI>();
+                if (messageTextComponent != null)
+                {
+                    if (i == 0)
+                    {
+                        messageTextComponent.text = player.name;
+                    }
+                    else if(i == 7)
+                    {
+                        // Total Score
+                    }
+                    else
+                    {
+                        messageTextComponent.text = player.m_score[i--];
+                    }
+                }
+
+                ++i;
+            }
+
+            Canvas.ForceUpdateCanvases();
+        }
+
+        #endregion
+
+        #region DebugWindow
 
         void TogleDebugWindow()
         {
